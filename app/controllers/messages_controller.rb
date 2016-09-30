75a0adad2fa748ea386f92a79a6d43302a2a7d16
emesc <tahder77@gmail.com>
2016-09-30 12:16:41 +1000

    Added new and create action and views.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,43 @@
 class MessagesController < ApplicationController
 
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
+
   def index
     @messages = Message.order("created_at DESC")
   end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @message.update_attributes(message_params)
+      redirect_to message_path(@message), notice: "Message updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: "Message deleted successfully."
+  end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:title, :description)
+    end
 end
